252084cd9deff5c421b5e6be80bb5eabb0652d94
Vibhuti Chandrakar <111688295+VC6378@users.noreply.github.com>
2024-05-08 15:22:40 +0530

    Time: 0 ms (100.00%), Space: 12.1 MB (77.38%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<string> ans;
+    
+    void f(string &s,int open,int close){
+        if(open==0 && close==0){
+            ans.push_back(s);
+            return;
+        }
+        if(open>0){
+            s.push_back('(');
+            f(s,open-1,close);
+            s.pop_back();
+        }
+        if(close>0){
+            if(open<close){
+                s.push_back(')');
+                f(s,open,close-1);
+                s.pop_back();
+            }
+        }
+    }
+    
+    vector<string> generateParenthesis(int n) {
+        string s;
+        f(s,n,n);
+        return ans;
+    }
+};
