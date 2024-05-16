380d15d52958f0e78c32c384fbf866e6be608c07
Vibhuti Chandrakar <111688295+VC6378@users.noreply.github.com>
2024-05-17 00:18:44 +0530

    Time: 57 ms (51.72%), Space: 31 MB (42.09%) - LeetHub

--- 3138-minimum-length-of-anagram-concatenation/3138-minimum-length-of-anagram-concatenation.cpp
+++ 3138-minimum-length-of-anagram-concatenation/3138-minimum-length-of-anagram-concatenation.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
     
-    bool f(string s, int k) {
-        int n = s.length();
-        int cnt[26] = {0};
-        for (int i = 0; i < k; i++)
-            cnt[s[i] - 'a']++;
-        for (int i = k; i < n; i += k) {
-            int cnt2[26] = {0};
-            for (int j = i; j < i + k; j++)
-                cnt2[s[j] - 'a']++;
-            for (int j = 0; j < 26; j++)
-                if (cnt[j] != cnt2[j])
-                    return false;
+    
+    bool f(string s,int k){
+        int n=s.size();
+        int cnt[26]={0};
+        for(int i=0;i<k;i++){
+            cnt[s[i]-'a']++;
+        }
+        for(int i=k;i<n;i+=k){
+            int cnt2[26]={0};
+            for(int j=i;j<i+k;j++){
+                cnt2[s[j]-'a']++;
+            }
+            for(int j=0;j<26;j++){
+                if(cnt[j]!=cnt2[j]) return false;
+            }
         }
         return true;
     }
@@ -20,9 +23,10 @@ public:
     int minAnagramLength(string s) {
         int n=s.size();
         for(int i=1;i<=n;i++){
-            if(n%i==0 && f(s,i)) return i;
+            if(n%i==0 && f(s,i)){
+                return i;
+            }
         }
         return -1;
-        
     }
 };
