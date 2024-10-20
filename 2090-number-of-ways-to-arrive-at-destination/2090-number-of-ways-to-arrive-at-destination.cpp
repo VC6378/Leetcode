d85db320c871210d21f141e060b9c54cd5f8b317
Vibhuti Chandrakar <111688295+VC6378@users.noreply.github.com>
2024-10-20 22:23:08 +0530

    Time: 17 ms (99.74%), Space: 39.4 MB (17.09%) - LeetHub

--- 2090-number-of-ways-to-arrive-at-destination/2090-number-of-ways-to-arrive-at-destination.cpp
+++ 2090-number-of-ways-to-arrive-at-destination/2090-number-of-ways-to-arrive-at-destination.cpp
@@ -6,33 +6,35 @@ public:
             adj[it[0]].push_back({it[1],it[2]});
             adj[it[1]].push_back({it[0],it[2]});
         }
-        vector<long long> dist(n,1e15);
+        vector<long long> dis(n,1e15);
         vector<long long> ways(n,0);
-
-        dist[0]=0;
+        dis[0]=0;
         ways[0]=1;
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+        priority_queue<pair<long,long>,vector<pair<long,long>>,
+        greater<pair<long,long>>>pq;
         pq.push({0,0});
-        int mod=(int)(1e9+7);
+        int mod = (int)(1e9+7);
         while(!pq.empty()){
-            auto ptr=pq.top();
-            int distance=ptr.first;
-            int node=ptr.second;
+            auto ptr = pq.top();
+            long long distance = ptr.first;
+            long long node = ptr.second;
+
             pq.pop();
             for(auto it:adj[node]){
-                long long adjnode=it.first;
+                long long adjnode = it.first;
                 long long edgewt=it.second;
-                if(distance+edgewt<dist[adjnode]){
-                    dist[adjnode]=distance+edgewt;
+
+                if(distance+edgewt<dis[adjnode]){
+                    dis[adjnode]=distance+edgewt;
                     pq.push({distance+edgewt,adjnode});
                     ways[adjnode]=ways[node];
                 }
-                else if(distance+edgewt==dist[adjnode]){
+                else if(distance+edgewt==dis[adjnode]){
                     ways[adjnode]=(ways[adjnode]+ways[node])%mod;
                 }
             }
         }
-
         return ways[n-1]%mod;
+
     }
 };
