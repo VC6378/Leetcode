55f8a7cee3b06b9c53a11ea430c7cecc7cc0591e
Vibhuti Chandrakar <111688295+VC6378@users.noreply.github.com>
2024-10-20 17:46:58 +0530

    Time: 15 ms (99.8%), Space: 38.2 MB (33.5%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -5,36 +5,30 @@ public:
         int n=mat[0].size();
         vector<vector<int>> vis(m,vector<int>(n,0));
         vector<vector<int>> dis(m,vector<int>(n,0));
-        queue<pair<pair<int,int>,int>>q;
+        queue<pair<pair<int,int>,int>> q;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(mat[i][j]==0){
                     q.push({{i,j},0});
                     vis[i][j]=1;
                 }
-                else{
-                    vis[i][j]=0;
-                }
             }
         }
 
         int delrow[]={-1,0,+1,0};
         int delcol[]={0,+1,0,-1};
-
         while(!q.empty()){
             int row=q.front().first.first;
             int col=q.front().first.second;
-            int steps=q.front().second;
+            int dist=q.front().second;
+            dis[row][col]=dist;
             q.pop();
-
-            dis[row][col]=steps;
             for(int i=0;i<4;i++){
                 int nrow=row+delrow[i];
                 int ncol=col+delcol[i];
-
-                if(nrow>=0 and nrow<m and ncol>=0 and ncol<n and !vis[nrow][ncol]){
+                if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && !vis[nrow][ncol]){
                     vis[nrow][ncol]=1;
-                    q.push({{nrow,ncol},steps+1});
+                    q.push({{nrow,ncol},dist+1});
                 }
             }
         }
