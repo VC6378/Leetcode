bf48d2c2a88a8cd46abb19f4a0ea18bbf86cfaa9
Vibhuti Chandrakar <111688295+VC6378@users.noreply.github.com>
2025-01-07 11:57:05 +0530

    Time: 569 ms (11.23%), Space: 394 MB (22.84%) - LeetHub

---
+++ 3678-design-task-manager/3678-design-task-manager.cpp
@@ -0,0 +1,50 @@
+class TaskManager {
+public:
+    set<pair<long long,int>> set ;
+    map<int , pair<long long,int>> map ;
+    TaskManager(vector<vector<int>>& tasks) {
+        for(auto i : tasks) {
+            int task = i[1] ;
+            int userId = i[0] ;
+            int prior = i[2] ;
+            map[task] = {prior , userId} ;
+            set.insert({prior , task}) ;
+        }
+    }
+    
+    void add(int userId, int task, int prior) {
+        map[task] = {prior , userId} ;
+        set.insert({prior , task}) ;
+    }
+    
+    void edit(int taskId, int newPriority) {
+        long long oldPrior = map[taskId].first ;
+        int userId = map[taskId].second ;
+        auto it = set.find({oldPrior , taskId}) ;
+        set.erase(it) ;
+        set.insert({newPriority , taskId}) ;
+        map[taskId].first = newPriority ;
+    }
+    
+    void rmv(int taskId) {
+        long long oldPrior = map[taskId].first ;
+        int userId = map[taskId].second ;
+        auto it = set.find({oldPrior , taskId}) ;
+        set.erase(it) ;
+        map.erase(taskId) ;
+    }
+    
+    int execTop() {
+        if(set.size() == 0 ) {
+            return -1 ;
+        }
+        auto it = set.rbegin() ;
+        pair<long long , int> p = *it ;
+        //cout << p.first << " " << p.second ;
+        // rmv(p.second) ;
+        int val =  map[p.second].second ;
+        rmv(p.second) ;
+        return val ;
+    }
+};
+
