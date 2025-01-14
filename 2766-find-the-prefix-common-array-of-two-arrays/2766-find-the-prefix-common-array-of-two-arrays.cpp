c235726a385973288ecec905cf0613ff69b34151
Vibhuti Chandrakar <111688295+VC6378@users.noreply.github.com>
2025-01-14 22:30:30 +0530

    Time: 0 ms (100%), Space: 85.9 MB (76.89%) - LeetHub

--- 2766-find-the-prefix-common-array-of-two-arrays/2766-find-the-prefix-common-array-of-two-arrays.cpp
+++ 2766-find-the-prefix-common-array-of-two-arrays/2766-find-the-prefix-common-array-of-two-arrays.cpp
@@ -6,15 +6,16 @@ public:
         vector<int> ans(n,0);
         int common=0;
         for(int i=0;i<n;i++){
-            
             freq[a[i]]++;
-            if(freq[a[i]]==2) common++;
+            if(freq[a[i]]==2){
+                common++;
+            }
 
             freq[b[i]]++;
             if(freq[b[i]]==2) common++;
-
             ans[i]=common;
         }
+
         return ans;
     }
 };
