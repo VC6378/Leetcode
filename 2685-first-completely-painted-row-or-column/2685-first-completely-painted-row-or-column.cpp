4902b799bb0df894a200d6d332a58d3080039464
Vibhuti Chandrakar <111688295+VC6378@users.noreply.github.com>
2025-01-20 21:19:12 +0530

    Time: 93 ms (68.6%), Space: 170.9 MB (37.8%) - LeetHub

--- 2685-first-completely-painted-row-or-column/2685-first-completely-painted-row-or-column.cpp
+++ 2685-first-completely-painted-row-or-column/2685-first-completely-painted-row-or-column.cpp
@@ -4,6 +4,9 @@ public:
         int si=arr.size();
         int m=mat.size();
         int n=mat[0].size();
+
+        vector<int> rowCount(m,n);
+        vector<int> colCount(n,m);
         unordered_map<int,pair<int,int>> mp;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
@@ -11,14 +14,14 @@ public:
             }
         }
 
-        vector<int> rowCount(m, n), colCount(n, m);
-
         for(int i=0;i<si;i++){
-            int val=arr[i];
+            int val = arr[i];
             int row = mp[val].first;
             int col = mp[val].second;
+
             rowCount[row]--;
             colCount[col]--;
+
             if(rowCount[row]==0 or colCount[col]==0) return i;
         }
         return -1;
