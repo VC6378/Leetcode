98042f578277b101a86f4e7f64d9428ddd0ca593
Vibhuti Chandrakar <111688295+VC6378@users.noreply.github.com>
2025-01-22 20:45:29 +0530

    Time: 67 ms (88.83%), Space: 112.9 MB (75.24%) - LeetHub

--- 1876-map-of-highest-peak/1876-map-of-highest-peak.cpp
+++ 1876-map-of-highest-peak/1876-map-of-highest-peak.cpp
@@ -3,6 +3,7 @@ public:
     vector<vector<int>> highestPeak(vector<vector<int>>& water) {
         int m=water.size();
         int n=water[0].size();
+
         vector<vector<int>> ans(m,vector<int>(n,-1));
         queue<pair<int,int>> q;
         for(int i=0;i<m;i++){
@@ -14,25 +15,22 @@ public:
             }
         }
 
-        int dr[]={0,+1,0,-1};
+        int dr[]={0,-1,0,+1};
         int dc[]={+1,0,-1,0};
 
         while(!q.empty()){
-            int row = q.front().first;
-            int col = q.front().second;
+            int row=q.front().first;
+            int col=q.front().second;
             q.pop();
             for(int i=0;i<4;i++){
                 int nrow=row+dr[i];
                 int ncol=col+dc[i];
-
                 if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && ans[nrow][ncol]==-1){
                     ans[nrow][ncol]=1+ans[row][col];
                     q.push({nrow,ncol});
                 }
             }
-
         }
-
         return ans;
     }
 };
