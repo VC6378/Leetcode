5670337e74eb81fdeed0bc27b869a6f3ed9bee44
Vibhuti Chandrakar <111688295+VC6378@users.noreply.github.com>
2025-01-29 00:14:54 +0530

    Time: 3 ms (84.06%), Space: 96.5 MB (56.04%) - LeetHub

--- 2764-maximum-number-of-fish-in-a-grid/2764-maximum-number-of-fish-in-a-grid.cpp
+++ 2764-maximum-number-of-fish-in-a-grid/2764-maximum-number-of-fish-in-a-grid.cpp
@@ -21,7 +21,7 @@ public:
         int ans=0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                ans = max(ans,solve(i,j,grid,vis));
+                ans=max(ans,solve(i,j,grid,vis));
             }
         }
         return ans;
