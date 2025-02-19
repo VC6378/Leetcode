56f51c6782092d8fcc2d3d23400b053e26f1aa30
Vibhuti Chandrakar <111688295+VC6378@users.noreply.github.com>
2025-02-20 00:08:07 +0530

    Time: 196 ms (85.31%), Space: 10.4 MB (94.98%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
-    
-    bool f(vector<vector<char>> &board,string word,int row,int col,int ind){
+
+    bool f(vector<vector<char>> &board,int i,int j,string &word,int ind){
         if(ind==word.size()) return true;
-        int n=board.size();
-        int m=board[0].size();
-        if(row<0 || row>=n || col<0 || col>=m || word[ind]!=board[row][col]) return false;
-        char temp=board[row][col];
-        board[row][col]='0';
-        if(f(board,word,row-1,col,ind+1) or f(board,word,row+1,col,ind+1) or f(board,word,row,col+1,ind+1) or f(board,word,row,col-1,ind+1)) return true;
-        board[row][col]=temp;
+        int m=board.size();
+        int n=board[0].size();
+        if(i<0 || i>=m || j<0 || j>=n || board[i][j]!=word[ind]) return false;
+        char temp = board[i][j];
+        board[i][j]='0';
+        if(f(board,i-1,j,word,ind+1) || f(board,i+1,j,word,ind+1) || f(board,i,j+1,word,ind+1) || f(board,i,j-1,word,ind+1)) return true;
+        board[i][j]=temp;
         return false;
     }
-    
+
     bool exist(vector<vector<char>>& board, string word) {
-        int n=board.size();
-        int m=board[0].size();
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(f(board,word,i,j,0)) return true;
+        int m=board.size();
+        int n=board[0].size();
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(f(board,i,j,word,0)) return true;
             }
         }
         return false;
