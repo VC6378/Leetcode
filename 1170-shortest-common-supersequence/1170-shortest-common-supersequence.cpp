9580edc8f3e47620ae685d559c099ee4eec45467
Vibhuti Chandrakar <111688295+VC6378@users.noreply.github.com>
2025-02-28 16:52:59 +0530

    Time: 27 ms (38.39%), Space: 26.8 MB (43.64%) - LeetHub

--- 1170-shortest-common-supersequence/1170-shortest-common-supersequence.cpp
+++ 1170-shortest-common-supersequence/1170-shortest-common-supersequence.cpp
@@ -7,17 +7,16 @@ public:
         for(int i=1;i<=m;i++){
             for(int j=1;j<=n;j++){
                 if(one[i-1]==two[j-1]){
-                    dp[i][j]=1+dp[i-1][j-1];
+                    dp[i][j] = 1+dp[i-1][j-1];
                 }
                 else{
-                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                    dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
                 }
             }
         }
-
         int i=m;
         int j=n;
-        string ans="";
+        string ans = "";
         while(i>0 && j>0){
             if(one[i-1]==two[j-1]){
                 ans+=one[i-1];
@@ -38,12 +37,10 @@ public:
             ans+=one[i-1];
             i--;
         }
-
         while(j>0){
             ans+=two[j-1];
             j--;
         }
-
         reverse(ans.begin(),ans.end());
         return ans;
     }
