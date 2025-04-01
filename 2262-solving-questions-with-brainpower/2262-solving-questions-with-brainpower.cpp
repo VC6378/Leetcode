0186e91eda68da1538d036d34e8f10647c533fa4
Vibhuti Chandrakar <111688295+VC6378@users.noreply.github.com>
2025-04-01 23:33:06 +0530

    Time: 11 ms (32.59%), Space: 127.7 MB (16.19%) - LeetHub

--- 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.cpp
+++ 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
 
-    long long f(int ind,vector<long long> &dp,vector<vector<int>> &ques){
-        int n=ques.size();
-        if(ind>=n) return 0;
+    long long f(int ind,vector<vector<int>> &que,vector<long long> &dp){
+        if(ind>=que.size()) return 0;
         if(dp[ind]!=-1) return dp[ind];
-        long long take = ques[ind][0]+f(ind+ques[ind][1]+1,dp,ques);
-        long long nottake = f(ind+1,dp,ques);
-        return max(take,nottake);
+        long long nottake=f(ind+1,que,dp);
+        long long take=que[ind][0]+f(ind+que[ind][1]+1,que,dp);
+        return dp[ind]=max(take,nottake);
     }
 
-    long long mostPoints(vector<vector<int>>& ques) {
-        int n=ques.size();
+    long long mostPoints(vector<vector<int>>& que) {
+        int n=que.size();
         vector<long long> dp(n,-1);
-        return f(0,dp,ques);
+        return f(0,que,dp);
     }
 };
